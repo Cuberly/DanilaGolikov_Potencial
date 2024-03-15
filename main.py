acd505c2b7bc192f076b6d8c114e1eba014c5001
Cuberly <danila.m.golikov@gmail.com>
2024-03-15 18:37:08 +0300

    теперь точно обновил первую задачу, предыдущий коммит был ошибкой :d

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import csv
 with open('scientist.txt', encoding='utf8', ) as file:
     reader = csv.reader(file, delimiter='#')
     answer = list(reader)[1:]
-    print(answer)
     preparation_date = {}
-    for ScientistName, preparation, date, componentsin in answer:
-        if date < preparation.get(date, 0):
-        count_class[level] = count_class.get(level, 0) + 1
-        sum_class[level] = sum_class.get(level, 0) + (int(score) if score != 'None' else 0)
+    print(answer)
+    for person in answer:
+        date = person[-2]
+        preparation = person[-3]
+        if date < preparation_date.get(preparation, '99999999999999999999'):
+            answer.remove(person)
+    print(answer)
